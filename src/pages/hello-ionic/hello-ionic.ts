c3ef87c16e7ce5fdeee418203b5ebe79c73b440e
Ethan <ethan@webcomm.com.tw>
2018-12-22 18:32:56 +0800

    Component practice - actionSheet , alert

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,114 @@
 import { Component } from '@angular/core';
 
+import { Platform, ActionSheetController, AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
-
+  constructor(
+    public platform: Platform,
+    public actionsheetCtrl: ActionSheetController,
+    public alertCtrl: AlertController
+  ) { }
+  // Compoent : Action Sheet
+  openMenu() {
+    const actionSheet = this.actionsheetCtrl.create({
+      title: 'Albums',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Paly',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+      ]
+    });
+    actionSheet.present();
+  }
+  // Component: Prompt Alert
+  showPromptAlert() {
+    const prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: 'Enter a name for this new album you areso keen on adding',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log(`Save clicked title: ${data.title}`);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+  // Component: Radio Alert
+  showRadioAlert() {
+    const radio = this.alertCtrl.create();
+    radio.setTitle('Lightsaber color');
+    radio.addInput({
+      type: 'radio',
+      label: 'Blue',
+      value: 'blue',
+      checked: true
+    });
+    radio.addInput({
+      type: 'radio',
+      label: 'Red',
+      value: 'red',
+      checked: false
+    });
+    radio.addButton('Cancel');
+    radio.addButton({
+      text: 'OK',
+      handler: data => {
+        console.log(`You choose ${data}`);
+      }
+    });
+    radio.present();
   }
 }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -17,7 +17,7 @@ export class ListPage {
     'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
-    for(let i = 1; i < 11; i++) {
+    for(let i = 1; i < 21; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
